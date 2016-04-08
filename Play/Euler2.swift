fbb855be9c66b0b508d9778bb7657f000e872086
Ben Woods <benwoods2650@gmail.com>
2016-04-08 20:56:20 +0100

    Second problem

---
+++ Play/Euler2.swift
@@ -0,0 +1,30 @@
+//
+//  Euler2.swift
+//  Play
+//
+//  Created by Ben Woods on 08/04/2016.
+//  Copyright © 2016 Ben Woods. All rights reserved.
+//
+
+import Foundation
+
+class Euler2 {
+    
+    // Get the sum of the even Fibonacci numbers under one million
+    func run() -> Int {
+        var current = 1
+        var minusOne = 1
+        var evenNumbers = [Int]()
+        
+        while(current <= 1000000){
+            if(current % 2 == 0){
+                evenNumbers.append(current)
+            }
+            let temp = current
+            current += minusOne
+            minusOne = temp
+        }
+        
+        return evenNumbers.reduce(0, combine: +);
+    }
+}
